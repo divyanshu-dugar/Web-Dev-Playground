e73fba536df6a0bd541db56445f9802c1142c787
Divyanshu Dugar <divyu2004@gmail.com>
2025-12-06 20:44:52 -0500

    built next.js snippet

---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+// app/page.tsx (or wherever you want to use it)
+import CodeSnippetDisplay from '@/components/CodeSnippetDisplay';
+
+export default function Home() {
+  return <CodeSnippetDisplay />;
+}
